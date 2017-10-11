578558dfa2dbcdd14d6343ca2f8df761e3d2b905
StormChaserCN <31496776+StormChaserCN@users.noreply.github.com>
2017-10-11 10:19:49 +0800

    修改部分BUG

--- main.py
+++ main.py
@@ -12,57 +12,57 @@ from Container.VideoContainer import *
 BUF_SIZE = 10485760  # 10MB
 
 
-def compress_file(srcpath, dstpath):
-    cobj = zlib.compressobj(9)
-    with open(srcpath, 'rb') as fdin, open(dstpath, 'wb') as fdout:
-        data = fdin.read(BUF_SIZE)
+def compress_file(src_path, dst_path):
+    compress_obj = zlib.compressobj(9)
+    with open(src_path, 'rb') as fd_in, open(dst_path, 'wb') as fd_out:
+        data = fd_in.read(BUF_SIZE)
         while data:
-            fdout.write(cobj.compress(data))
-            data = fdin.read(BUF_SIZE)
-        fdout.write(cobj.flush())
+            fd_out.write(compress_obj.compress(data))
+            data = fd_in.read(BUF_SIZE)
+        fd_out.write(compress_obj.flush())
 
 
-def decompress_file(srcpath, dstpath):
-    dobj = zlib.decompressobj()
-    with open(srcpath, 'rb') as fdin, open(dstpath, 'wb') as fdout:
-        data = fdin.read(BUF_SIZE)
+def decompress_file(src_path, dst_path):
+    decompress_obj = zlib.decompressobj()
+    with open(src_path, 'rb') as fd_in, open(dst_path, 'wb') as fd_out:
+        data = fd_in.read(BUF_SIZE)
         while data:
-            fdout.write(dobj.decompress(data))
-            data = fdin.read(BUF_SIZE)
-        fdout.write(dobj.flush())
+            fd_out.write(decompress_obj.decompress(data))
+            data = fd_in.read(BUF_SIZE)
+        fd_out.write(decompress_obj.flush())
 
 
-def encrypt_file(key, srcpath, dstpath):
+def encrypt_file(key, src_path, dst_path):
     iv = Random.new().read(AES.block_size)
-    filesize = os.path.getsize(srcpath)
+    file_size = os.path.getsize(src_path)
     m = MD5.new()
     m.update(key)
     key = m.digest()
     encryptor = AES.new(key, AES.MODE_CBC, iv)
-    with open(srcpath, 'rb') as fdin, open(dstpath, 'wb') as fdout:
-        fdout.write(iv)
-        fdout.write(struct.pack('<Q', filesize))
-        data = fdin.read(BUF_SIZE)
+    with open(src_path, 'rb') as fd_in, open(dst_path, 'wb') as fd_out:
+        fd_out.write(iv)
+        fd_out.write(struct.pack('<Q', file_size))
+        data = fd_in.read(BUF_SIZE)
         while data:
             if len(data) % AES.block_size != 0:
                 data += '\x00' * (AES.block_size - len(data) % AES.block_size)
-            fdout.write(encryptor.encrypt(data))
-            data = fdin.read(BUF_SIZE)
+            fd_out.write(encryptor.encrypt(data))
+            data = fd_in.read(BUF_SIZE)
 
 
-def decrypt_file(key, srcpath, dstpath):
+def decrypt_file(key, src_path, dst_path):
     m = MD5.new()
     m.update(key)
     key = m.digest()
-    with open(srcpath, 'rb') as fdin, open(dstpath, 'wb') as fdout:
-        iv = fdin.read(AES.block_size)
-        origsize = struct.unpack('<Q', fdin.read(struct.calcsize('Q')))[0]
+    with open(src_path, 'rb') as fd_in, open(dst_path, 'wb') as fd_out:
+        iv = fd_in.read(AES.block_size)
+        original_size = struct.unpack('<Q', fd_in.read(struct.calcsize('Q')))[0]
         decryptor = AES.new(key, AES.MODE_CBC, iv)
-        data = fdin.read(BUF_SIZE)
+        data = fd_in.read(BUF_SIZE)
         while data:
-            fdout.write(decryptor.decrypt(data))
-            data = fdin.read(BUF_SIZE)
-        fdout.truncate(origsize)
+            fd_out.write(decryptor.decrypt(data))
+            data = fd_in.read(BUF_SIZE)
+        fd_out.truncate(original_size)
 
 
 def smash_into(container, info_path, save_path, key, ver=0):
@@ -70,8 +70,8 @@ def smash_into(container, info_path, save_path, key, ver=0):
         compress_file(info_path, info_path + '.1')
         encrypt_file(key, info_path + '.1', info_path + '.2')
         file_size = os.path.getsize(info_path + '.2')
-        with open(info_path + '.2', 'rb') as fdfile:
-            info = fdfile.read(file_size)
+        with open(info_path + '.2', 'rb') as fd_file:
+            info = fd_file.read(file_size)
             if ver == 0:
                 container.smash_into(info, save_path)
             else:
